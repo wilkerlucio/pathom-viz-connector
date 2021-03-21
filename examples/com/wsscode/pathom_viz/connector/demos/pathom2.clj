b918ebae6cbc10da3500147cdc4feffa4b2cdf64
Wilker Lucio <wilkerlucio@gmail.com>
2021-03-21 20:55:51 -0300

    Update demos

--- examples/com/wsscode/pathom_viz/connector/demos/pathom2.clj
+++ examples/com/wsscode/pathom_viz/connector/demos/pathom2.clj
@@ -1,17 +1,24 @@
 (ns com.wsscode.pathom-viz.connector.demos.pathom2
   (:require [com.wsscode.pathom.viz.ws-connector.core :as p.connector]
             [com.wsscode.pathom.core :as p]
-            [com.wsscode.pathom.connect :as pc])
+            [com.wsscode.pathom.connect :as pc]
+            [com.wsscode.pathom3.connect.indexes :as pci]
+            [clojure.set :as set])
   (:import (java.time LocalDate)))
 
 (pc/defresolver nested [_]
   {:output {:nested {:value "true"}}})
 
+(pc/defresolver item-by-id [{:keys [item/id]}]
+  {:item/id   id
+   :item/text "bar"})
+
 (def registry
   [nested
    (pc/constantly-resolver :pi Math/PI)
    (pc/constantly-resolver :bad (LocalDate/now))
-   (pc/single-attr-resolver :pi :tau #(* 2 %))])
+   (pc/single-attr-resolver :pi :tau #(* 2 %))
+   item-by-id])
 
 (def parser
   (p/parser
@@ -25,13 +32,19 @@
                   p/error-handler-plugin
                   p/trace-plugin]}))
 
-(comment
-  (def tracked-parser
-    (p.connector/connect-parser
-      {::p.connector/parser-id ::my-parser}
-      (fn [env tx]
-        (parser (assoc env :extra-connector-stuff "bla") tx))))
+(def tracked-parser
+  (p.connector/connect-parser
+    {::p.connector/parser-id ::my-parser}
+    (fn [env tx]
+      (parser (assoc env :extra-connector-stuff "bla") tx))))
 
-  (parser {} [:tau :com.wsscode.pathom/trace])
+(comment
+
+
+  (parser {} [{::pc/indexes [::pc/index-io]}])
+
+  (pci/reachable-paths
+    #:com.wsscode.pathom3.connect.indexes{:index-io {#{:item/id} #:item{:id {}, :text {}}}}
+    {:item/id {}})
 
   (tracked-parser {} [:works-here?]))
--- examples/com/wsscode/pathom_viz/connector/demos/pathom3.clj
+++ examples/com/wsscode/pathom_viz/connector/demos/pathom3.clj
@@ -5,27 +5,77 @@
             [com.wsscode.pathom3.interface.smart-map :as psm]
             [com.wsscode.pathom.viz.ws-connector.core :as pvc]
             [com.wsscode.pathom3.connect.indexes :as pci]
-            [com.wsscode.pathom3.interface.async.eql :as p.a.eql])
+            [com.wsscode.pathom3.interface.async.eql :as p.a.eql]
+            [com.wsscode.pathom3.connect.operation :as pco]
+            [com.wsscode.pathom3.connect.runner :as pcr]
+            [com.wsscode.pathom3.connect.planner :as pcp]
+            [edn-query-language.core :as eql])
   (:import (java.time LocalDate)))
+
+(pco/defmutation message []
+  {::pco/op-name 'message}
+  {:response "true"})
+
+(pco/defmutation message-error []
+  {::pco/op-name 'message-error}
+  (throw (ex-info "Error here" {})))
 
 (def registry
   [(pbir/constantly-resolver :pi Math/PI)
    (pbir/constantly-resolver :bad (LocalDate/now))
    (pbir/constantly-resolver :list [{:id 1} {:id 2}])
+   (pbir/constantly-resolver :nest {:id 1})
    (pbir/constantly-fn-resolver :error (fn [_] (throw (ex-info "Error" {}))))
    (pbir/single-attr-resolver :pi :tau #(* 2 %))
-   (pbir/single-attr-resolver :id :x inc)])
+   (pbir/single-attr-resolver :id :x inc)
+   message
+   message-error])
+
+(defonce plan-cache* (atom {}))
 
 (def env
-  (pci/register registry))
+  (-> (pci/register registry)
+      (pcp/with-plan-cache plan-cache*)))
 
 (def tracked-env
   (-> env
-      (p.connector/connect-env {::pvc/parser-id "demo"})))
+      (p.connector/connect-env {::pvc/parser-id      `env
+                                ::p.connector/async? false})))
 
 (comment
+  (pcp/compute-plan-snapshots
+    (assoc env
+      ::pcp/available-data         {}
+      :edn-query-language.ast/node (eql/query->ast [:tau])))
+
+  (p.eql/process tracked-env
+    [(list 'com.wsscode.pathom.viz.ws-connector.pathom3/request-snapshots
+       {::pcp/available-data         {}
+        ::pcp/source-ast (eql/query->ast [:pi])})])
+
+  (meta (p.eql/process tracked-env
+          '[{(message {})
+             [:pi]}]))
+
   (p.eql/process tracked-env
     [:tau])
+
+  (-> (p.eql/process tracked-env
+        [{:list [:x]}])
+      meta)
+
+  (-> (p.eql/process tracked-env
+        [{:nest [:x]}])
+      meta)
+
+  (-> (p.eql/process tracked-env
+        [{[:id 123] [:x]}])
+      (get [:id 123])
+      (meta))
+
+  (p.eql/process tracked-env
+    {:items [{:id 1} {:id 2}]}
+    [{:items [:x]}])
 
   (p.eql/process tracked-env
     [:com.wsscode.pathom.connect/index-resolvers])
@@ -34,7 +84,7 @@
     [:com.wsscode.pathom.connect/idents])
 
   (-> (p.a.eql/process env
-     [{:list [:x]}])
+        [{:list [:x]}])
       deref
       meta)
 
